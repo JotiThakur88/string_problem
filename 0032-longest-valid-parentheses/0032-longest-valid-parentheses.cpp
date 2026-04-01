0663793f4699d0b9df03d4b446cc5dc8eda3eca9
Jyoti <151373033+JotiThakur88@users.noreply.github.com>
2026-04-01 11:13:52 +0530

    Time: 0 ms (100%), Space: 11.8 MB (22.74%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);  // base for length calculation
+        
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+
+                if (st.empty()) {
+                    st.push(i); // reset base index
+                } else {
+                    maxLen = max(maxLen, i - st.top());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
